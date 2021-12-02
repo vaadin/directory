e2e2bf8de449bc337e412ecaa67b7e60689dbd1f
Marcus Hellberg <marcus@vaadin.com>
2021-12-02 11:16:14 +0200

    styles

--- frontend/views/search-view.ts
+++ frontend/views/search-view.ts
@@ -1,11 +1,11 @@
 import '@vaadin/text-field';
 import '@vaadin/grid';
+import '@vaadin/icon';
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset';
 import '@vaadin/button';
 import './components/addon-card';
 import { html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-import '@vaadin/vaadin-lumo-styles/sizing';
-import '@vaadin/vaadin-lumo-styles/spacing';
 import { View } from './view';
 import Addon from 'Frontend/generated/org/vaadin/directory/search/Addon';
 import { SearchEndpoint } from 'Frontend/generated/endpoints';
@@ -28,7 +28,7 @@ export class SearchView extends View {
   render() {
     return html`
       <div class="flex flex-col items-center">
-        <h2>Vaadin directory</h2>
+        <h2 class="uppercase text-xs text-primary">Vaadin directory</h2>
         <h1>Add-on components and integrations</h1>
         <p>
           Search official and community add-ons and share your own to help
@@ -38,8 +38,9 @@ export class SearchView extends View {
       <h2>Browse All</h2>
       <div class="flex justify-between">
         <vaadin-text-field
-          label="Search"
+          placeholder="Search"
           @input=${(e: any) => (this.searchString = e.target.value)}>
+          <vaadin-icon slot="prefix" icon="vaadin:search"></vaadin-icon>
         </vaadin-text-field>
       </div>
       <div class="addons-grid">
