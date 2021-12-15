a66acddc72df72914d2f98d163d67b935f5f2e64
Marcus Hellberg <marcus@vaadin.com>
2021-12-15 08:19:12 -0800

    update to latest vaadin

--- src/main/java/org/vaadin/directory/endpoint/AddonEndpoint.java
+++ src/main/java/org/vaadin/directory/endpoint/AddonEndpoint.java
@@ -1,20 +1,20 @@
 package org.vaadin.directory.endpoint;
 
-import com.vaadin.directory.entity.directory.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.vaadin.flow.server.auth.AnonymousAllowed;
-import com.vaadin.fusion.Endpoint;
-import com.vaadin.fusion.Nonnull;
-import org.vaadin.directory.search.Addon;
-
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import com.vaadin.directory.backend.service.ComponentService;
 import com.vaadin.directory.entity.directory.Component;
-
-import java.time.LocalDate;
-import java.util.*;
-import java.time.ZoneId;
-import java.util.stream.Collectors;
+import com.vaadin.directory.entity.directory.Tag;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.fusion.Endpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.directory.search.Addon;
 
 @Endpoint
 @AnonymousAllowed
@@ -25,6 +25,7 @@ public class AddonEndpoint {
     AddonEndpoint(@Autowired ComponentService service) {
         this.service = service;
     }
+
     public Addon getAddon(String urlIdentifier) {
         Optional<Component> maybeComponent = service.getComponentByUrl(urlIdentifier);
         return maybeComponent.isPresent() ? componentToAddon(maybeComponent.get()) : null;
@@ -41,11 +42,13 @@ public class AddonEndpoint {
     }
 
     public static LocalDate dateToLocalDate(Date date) {
-        if (date == null) return LocalDate.of(2000,1,1);
+        if (date == null)
+            return LocalDate.of(2000, 1, 1);
         return date.toInstant()
                 .atZone(ZoneId.systemDefault())
                 .toLocalDate();
     }
+
     public static List<String> tagsToStrings(Set<Tag> tags) {
         if (tags == null) {
             return Collections.EMPTY_LIST;
--- src/main/java/org/vaadin/directory/endpoint/SearchEndpoint.java
+++ src/main/java/org/vaadin/directory/endpoint/SearchEndpoint.java
@@ -1,21 +1,16 @@
 package org.vaadin.directory.endpoint;
 
-import com.vaadin.flow.server.auth.AnonymousAllowed;
-import com.vaadin.fusion.Endpoint;
-import com.vaadin.fusion.Nonnull;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.vaadin.directory.backend.service.ComponentService;
-import com.vaadin.directory.entity.directory.Component;
-import com.vaadin.directory.entity.directory.ComponentDirectoryUser;
-import org.vaadin.directory.search.Addon;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import com.vaadin.directory.backend.service.ComponentService;
+import com.vaadin.directory.entity.directory.Component;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.fusion.Endpoint;
+import com.vaadin.fusion.Nonnull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.vaadin.directory.search.Addon;
 
 @Endpoint
 @AnonymousAllowed
@@ -28,7 +23,7 @@ public class SearchEndpoint {
 
     public SearchEndpoint(@Autowired ComponentService service) {
         this.service = service;
-        allComponents = service.findAllPublishedComponents(PageRequest.of(0, 2000));
+        allComponents = service.findAllPublishedComponents(PageRequest.of(0, 10));
         allAddons = allComponents.stream()
                 .map(c -> AddonEndpoint.componentToAddon(c))
                 .collect(Collectors.toList());
