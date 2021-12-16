0b91a01255fd02c0c74b67d463b8cc2233a62caa
Sami Ekblad <sami@vaadin.com>
2021-12-16 14:54:37 +0200

    Removed unused imports

--- src/main/java/org/vaadin/directory/Application.java
+++ src/main/java/org/vaadin/directory/Application.java
@@ -2,16 +2,13 @@ package org.vaadin.directory;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
-import org.eclipse.persistence.internal.jpa.transaction.TransactionManagerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 /**
  * The entry point of the Spring Boot application.
