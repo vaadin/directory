387578c8614617c483a416684871ec620c600aa6
Sami Ekblad <sami@vaadin.com>
2021-12-16 15:53:24 +0200

    Added placeholder for install panel

--- frontend/views/addon/addon-view.ts
+++ frontend/views/addon/addon-view.ts
@@ -1,5 +1,6 @@
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-grid/vaadin-grid';
+import '@vaadin/vaadin-select';
 import { html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import '@vaadin/vaadin-lumo-styles/sizing';
@@ -42,23 +43,61 @@ export class AddonView extends View implements BeforeEnterObserver {
     }
 
     return html`
-      <div>
-        <h1>${this.addon.name}</h1>
-        <div>${this.addon.author} last updated ${this.addon.lastUpdated}</div>
-                  <div class="flex flex-row-reverse gap-xs flex-wrap">
-                    ${this.addon.tags.map(
-                      (tag) =>
-                        html`
-                          <vaadin-button
-                            @click=${() => this.searchByTag(tag)}
-                            theme="small">
-                            ${tag}
-                          </vaadin-button>
-                        `
-                    )}
-                  </div>
-        <p>${this.addon.summary}</p>
-        ${unsafeHTML(DomPurify.sanitize(marked.parse(this.addon.description)))}
+      <div class="flex gap-l">
+        <div class="flex flex-col gap-xs flex-wrap">
+            <h1>${this.addon.name}</h1>
+            <div>${this.addon.author} last updated ${this.addon.lastUpdated}</div>
+                      <div class="flex gap-xs flex-wrap">
+                        ${this.addon.tags.map(
+                          (tag) =>
+                            html`
+                              <vaadin-button
+                                @click=${() => this.searchByTag(tag)}
+                                theme="small">
+                                ${tag}
+                              </vaadin-button>
+                            `
+                        )}
+                      </div>
+            <p>${this.addon.summary}</p>
+            ${unsafeHTML(DomPurify.sanitize(marked.parse(this.addon.description)))}
+          </div>
+          <div class="bg-primary-10 shadow-xs row-span-2 m-s p-m">
+              <h3>Install</h3>
+              <p>
+                  Add-on version <br />
+                  <select label="Add-on version">
+                      <option>Version 2.0.6</option>
+                      <option>Version 2.0.5</option>
+                      <option>Version 2.0.4</option>
+                      <option>Version 1.0.5</option>
+                  </select>
+              </p>
+              <p>
+                <a class="size-xs" href="">Link to this version</a>
+              </p>
+              <p>
+                Experimental<br />
+                Released 15 December 2021<br />
+                Apache License 2.0<br />
+              </p>
+              <h3>Framework support</h3>
+              <p>
+                Vaadin platform 20+
+              </p>
+              <h3>Browser compatibility</h3>
+              <p>
+              Browser independent
+              </p>
+              <h3>Install with</h3>
+              <p>
+                <vaadin-button>Log in to install</vaadin-button>
+              </p>
+              <h3>Release notes - Version 2.0.6</h3>
+              <p>
+              Fixed bugs in rendering.
+              </p>
+          </div>
       </div>
     `;
   }
