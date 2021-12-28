8c07f589ce80a0e3ea9db5e91d95829a7f3d0e2e
Sami Ekblad <sami@vaadin.com>
2021-12-28 15:18:28 +0200

    VersionParser wip

---
+++ src/main/java/org/vaadin/directory/endpoint/search/VersionParser.java
@@ -0,0 +1,9 @@
+package org.vaadin.directory.endpoint.search;
+
+public class VersionParser {
+
+    public static VersionParser parse(String versionSring) {
+        return new VersionParser();
+    }
+
+}
