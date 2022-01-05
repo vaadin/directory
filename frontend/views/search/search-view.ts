638b7200a744eb73b2addcf56304cdd5349797d9
Marcus Hellberg <marcus@vaadin.com>
2022-01-04 18:50:36 -0800

    updated slider setup

--- frontend/views/search/search-view.ts
+++ frontend/views/search/search-view.ts
@@ -11,18 +11,15 @@ import { FilterAddedEvent } from './filter-added-event';
 import { searchStore } from './search-store';
 import { appStore } from 'Frontend/stores/app-store';
 import { AppEndpoint } from 'Frontend/generated/endpoints';
-import VersionInfo from 'Frontend/generated/org/vaadin/directory/endpoint/app/VersionInfo';
-import 'keen-slider/keen-slider.min.css'
+import 'keen-slider/keen-slider.min.css';
 import KeenSlider from 'keen-slider';
 import type { KeenSliderInstance } from 'keen-slider';
-import { observe } from "mobx"
-import { disqusReset } from "../disqus"
+import { disqusReset } from '../disqus';
 
 @customElement('search-view')
 export class SearchView extends View {
-
   @property()
-  private versionInfo: string = "(build info)"
+  private versionInfo: string = '(build info)';
 
   featuredSlider?: KeenSliderInstance;
 
@@ -30,20 +27,33 @@ export class SearchView extends View {
     super();
     appStore.currentViewTitle = 'Search';
   }
+
   render() {
     return html`
       <div class="flex flex-col items-center">
-        <span id="build-info"><a target="dgithub" href="https://github.com/vaadin/directory/commits/main">${this.versionInfo}</a></span>
+        <span id="build-info"
+          ><a
+            target="dgithub"
+            href="https://github.com/vaadin/directory/commits/main"
+            >${this.versionInfo}</a
+          ></span
+        >
         <h1>Add-ons, cool widgets, and integrations for Vaadin</h1>
       </div>
       <div id="featured-list" class="keen-slider">
         ${searchStore.featured.map(
-          (addon,i) => html` <div class="keen-slider__slide number-slide${i}"><addon-card .addon=${addon}></addon-card></div> `
+          (addon, i) =>
+            html`
+              <div class="keen-slider__slide number-slide${i}">
+                <addon-card .addon=${addon}></addon-card>
+              </div>
+            `
         )}
-        ${this.setupFeaturedSliderIfNeeded()}
       </div>
       <p>
-        Want to publish your work here? Great! <a href="javascript:window.haas.login()">Log in</a> and <a href="https://vaadin.com/directory/help">follow the instructions</a>.
+        Want to publish your work here? Great!
+        <a href="javascript:window.haas.login()">Log in</a> and
+        <a href="https://vaadin.com/directory/help">follow the instructions</a>.
       </p>
       <h2>Search for add-ons</h2>
       <vaadin-text-field
@@ -53,7 +63,13 @@ export class SearchView extends View {
         @change=${this.updateQuery}
         clear-button-visible>
       </vaadin-text-field>
-      <div>Found total <b>${searchStore.totalCount}</b> add-ons. <i class="text-2xs">Want to narrow down? Try filters like <a href="?q=v%3A8">v:8</a> or <a href="?q=author%3Ame">author:me</a></i></div>
+      <div>
+        Found total <b>${searchStore.totalCount}</b> add-ons.
+        <i class="text-2xs"
+          >Want to narrow down? Try filters like <a href="?q=v%3A8">v:8</a> or
+          <a href="?q=author%3Ame">author:me</a></i
+        >
+      </div>
       <div class="addons-grid" @filter-added=${this.filterAdded}>
         ${searchStore.addons.map(
           (addon) => html` <addon-card .addon=${addon}></addon-card> `
@@ -70,70 +86,83 @@ export class SearchView extends View {
     `;
   }
 
-  firstUpdated() {
+  async firstUpdated() {
     searchStore.init();
     this.setupIntersectionObserver();
 
-    AppEndpoint.getVersionInfo().then(v => {
-      this.versionInfo = v.version +" / " + v.buildTime + " / " + v.startTime;
-    });
+    const v = await AppEndpoint.getVersionInfo();
+    this.versionInfo = v.version + ' / ' + v.buildTime + ' / ' + v.startTime;
 
     // Reset discuss thread
-    disqusReset("search", "https://directory4.demo.vaadin.com", "Vaadin Directory Search", false);
-    this.setupFeaturedSliderIfNeeded();
+    disqusReset(
+      'search',
+      'https://directory4.demo.vaadin.com',
+      'Vaadin Directory Search',
+      false
+    );
     this.restoreScrollIfNeeded();
+
+    // Any time the featured addons change, setup slider
+    this.autorun(() => {
+      if (searchStore.featured.length > 0) {
+        // Schedule setup after render is complete
+        setTimeout(() => this.setupFeaturedSliderIfNeeded(), 0);
+      }
+    });
   }
 
   restoreScrollIfNeeded() {
     if (window.searchScroll && window.searchScroll > 0) {
-        // TODO: Really, this trick again...
-        setTimeout(function () { window.scroll(0, window.searchScroll);  }, 500);
+      // TODO: Really, this trick again...
+      setTimeout(function () {
+        window.scroll(0, window.searchScroll);
+      }, 500);
     }
   }
 
   setupFeaturedSliderIfNeeded() {
-    const elem: HTMLElement = <HTMLElement>this.renderRoot.querySelector("#featured-list");
-    if (!elem
-        || elem.childElementCount <= 0
-        || (this.featuredSlider && elem.childElementCount === this.featuredSlider.slides.length)) return;
-    this.featuredSlider = new KeenSlider(elem, {
-      initial: 0,
-      loop: true,
-      slides: {
-        perView: 1,
-        spacing: 10,
-      }
-    },
-    [
-      (slider) => {
-        let timeout : any;
-        let mouseOver = false
-        function clearNextTimeout() {
-          clearTimeout(timeout)
-        }
-        function nextTimeout() {
-          clearTimeout(timeout)
-          if (mouseOver) return
-          timeout = setTimeout(() => {
-            slider.next()
-          }, 4000)
-        }
-        slider.on("created", () => {
-          slider.container.addEventListener("mouseover", () => {
-            mouseOver = true
-            clearNextTimeout()
-          })
-          slider.container.addEventListener("mouseout", () => {
-            mouseOver = false
-            nextTimeout()
-          })
-          nextTimeout()
-        })
-        slider.on("dragStarted", clearNextTimeout)
-        slider.on("animationEnded", nextTimeout)
-        slider.on("updated", nextTimeout)
+    const elem = this.renderRoot.querySelector('#featured-list') as HTMLElement;
+
+    this.featuredSlider = new KeenSlider(
+      elem,
+      {
+        initial: 0,
+        loop: true,
+        slides: {
+          perView: 1,
+          spacing: 10,
+        },
       },
-    ]
+      [
+        (slider) => {
+          let timeout: any;
+          let mouseOver = false;
+          function clearNextTimeout() {
+            clearTimeout(timeout);
+          }
+          function nextTimeout() {
+            clearTimeout(timeout);
+            if (mouseOver) return;
+            timeout = setTimeout(() => {
+              slider.next();
+            }, 4000);
+          }
+          slider.on('created', () => {
+            slider.container.addEventListener('mouseover', () => {
+              mouseOver = true;
+              clearNextTimeout();
+            });
+            slider.container.addEventListener('mouseout', () => {
+              mouseOver = false;
+              nextTimeout();
+            });
+            nextTimeout();
+          });
+          slider.on('dragStarted', clearNextTimeout);
+          slider.on('animationEnded', nextTimeout);
+          slider.on('updated', nextTimeout);
+        },
+      ]
     );
   }
 
