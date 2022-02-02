46e1da3ab9a1a0a96b2d26468e2be106b85842c0
Sami Ekblad <sami@vaadin.com>
2022-02-02 16:46:45 +0200

    Display real name in addon view

--- src/main/java/org/vaadin/directory/endpoint/addon/AddonEndpoint.java
+++ src/main/java/org/vaadin/directory/endpoint/addon/AddonEndpoint.java
@@ -2,6 +2,7 @@ package org.vaadin.directory.endpoint.addon;
 
 import java.util.Optional;
 import com.vaadin.directory.backend.service.ComponentService;
+import com.vaadin.directory.backend.service.UserInfoService;
 import com.vaadin.directory.entity.directory.Component;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.fusion.Endpoint;
@@ -12,15 +13,25 @@ import org.springframework.transaction.annotation.Transactional;
 @AnonymousAllowed
 public class AddonEndpoint {
 
-    private ComponentService service;
+    private final UserInfoService userNameService;
+    private final ComponentService service;
 
-    AddonEndpoint(@Autowired ComponentService service) {
+    AddonEndpoint(@Autowired ComponentService service,
+                  @Autowired UserInfoService userNameService) {
         this.service = service;
+        this.userNameService = userNameService;
     }
 
     @Transactional(readOnly = true)
     public Addon getAddon(String urlIdentifier) {
         Optional<Component> maybeComponent = service.getComponentByUrl(urlIdentifier);
-        return maybeComponent.isPresent() ? new Addon(maybeComponent.get()) : null;
+        return maybeComponent.isPresent() ? createAddon(maybeComponent.get()) : null;
     }
+
+    private Addon createAddon(Component c) {
+        Addon a = new Addon(c);
+        a.setAuthor(userNameService.getNameforId(c.getOwner().getId()));
+        return a;
+    }
+
 }
