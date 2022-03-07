82bf884fffc49d6a47d8c6eb5b808b4740a4dd4e
Sami Ekblad <sami@vaadin.com>
2022-03-07 20:36:51 +0200

    Collapse compatibility like suggested in #2

--- frontend/views/addon/feature-matrix.ts
+++ frontend/views/addon/feature-matrix.ts
@@ -44,13 +44,37 @@ export class FeatureMatrix extends View {
           )}
         </tr>
           ${this.matrix.rows?.map((title,row) =>
-            html`<tr class="row-${row} ${title?.indexOf('(')==0?'not-supported-fw':''}"><th class="rowh">${title}</th>${this.matrix?.data ?
+            html`<tr class="row row-${row} ${title?.indexOf('-')==0?'collapsed':''} ${title?.indexOf('(')==0?'not-supported-fw':''}"><th class="rowh">${title}</th>${this.matrix?.data ?
                 (this.matrix.data[row] ? this.matrix.data[row]?.map((content:string|undefined,col:number|undefined) =>
                     html`<td class="data row-${row} col-${col} ${content ? 'on':'off'}">${content}</td>`): html``) :
                 html`<td colspan="${this.matrix?.cols.length}"></td>`}</tr>`
           )}
       </table>
       `;
+  }
+
+  preProcess(input: Matrix) {
+    //input.forEach
+    let combined = JSON.stringify(input.data[0]);
+    let combinedi = 0;
+    input.data.forEach((d,i) => {
+      if (i > 0) {
+        const supported =  !((''+input.rows[i]).indexOf('(') === 0);
+        if (JSON.stringify(d) === combined && supported) {
+          input.rows[i] = '-'+input.rows[i];
+        } else {
+          // no match, close te combined if open
+          if (combinedi < i-1) {
+            input.rows[combinedi] = (''+input.rows[i-1]).substring(1) + '-' +input.rows[combinedi];
+          }
+          combined = JSON.stringify(d);
+          combinedi = i;
+        }
+      }
+
+
+    });
+    return input;
   }
 
   initIntersectionObserver() {
@@ -68,7 +92,7 @@ export class FeatureMatrix extends View {
         // Toggle state
         if (entry.isIntersecting != this.intersecting) this.intersecting = entry.isIntersecting;
         if (this.intersecting && !this.matrix) {
-          SearchEndpoint.getCompatibility(this.addon).then(value => this.matrix = value);
+          SearchEndpoint.getCompatibility(this.addon).then(value => this.matrix = this.preProcess(value));
         }
       });
     }, { rootMargin: "50px" }); // 50px margin
