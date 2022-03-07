f0f6a707d33dbdc7865c4e4b60aa93b35f69aac1
Sami Ekblad <sami@vaadin.com>
2022-03-07 15:39:07 +0200

    Implement carousel as described in #4

--- frontend/views/addon/highlight-carousel.ts
+++ frontend/views/addon/highlight-carousel.ts
@@ -2,6 +2,9 @@ import Addon from 'Frontend/generated/org/vaadin/directory/endpoint/addon/Addon'
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { View } from '../view';
+import 'keen-slider/keen-slider.min.css';
+import KeenSlider from 'keen-slider';
+import type { KeenSliderInstance } from 'keen-slider';
 
 @customElement('highlight-carousel')
 export class HighlightCarousel extends View {
@@ -9,51 +12,102 @@ export class HighlightCarousel extends View {
   @property({ attribute: false })
   addon?: Addon;
 
+  slider?: KeenSliderInstance;
+
+  sliderElement?: HTMLElement;
+  arElement?: HTMLElement;
+  alElement?: HTMLElement;
+
   firstUpdated() {
+    this.sliderElement = this.querySelector("#slider") as HTMLElement;
+    this.alElement = this.querySelector("#arrow-left") as HTMLElement;
+    this.arElement = this.querySelector("#arrow-right") as HTMLElement;
     this.classList.toggle('empty', this.addon?.mediaHighlights.length === 0);
+    this.addEventListener('keydown', this.keydown);
   }
 
   render() {
-    return html`
-      ${this.addon?.mediaHighlights.map((m,i) => html`
-        <div class="item item-${i}" @keydown="${this.closeOnEsc}" tabindex="0">
-          <img src="${m?.url}" @click="${this.close}">
-          <button class="btn-open" @click="${this.open}">View larger</button>
+    return html`<div id="slider" tabindex="0">
+        ${this.addon?.mediaHighlights.map((m,i) => html`
+          <div class="item number-slide${i}">
+            <img src="${m?.url}">
+            <button class="btn-open" @click="${this.open}">View larger</button>
+          </div>
           <button class="btn-close" @click="${this.close}">Close</button>
-        </div>
-      `)}
+        `)}
+      </div>
+      <div class="arrow" id="arrow-left" @click="${this.prev}"></div>
+      <div class="arrow" id="arrow-right" @click="${this.next}"></div>
     `;
   }
 
   open(e:MouseEvent) {
     const el = e.target as HTMLButtonElement;
     const item = el.parentElement;
+    const main = this;
     if (item) {
 
-      // Pop up from mouse x/y
-      item.style.setProperty('--x', ''+e.clientX+'px');
-      item.style.setProperty('--y', ''+e.clientY+'px');
-      item.classList.add('item-open-start');
+      // Select the slide that was clicked
+      let slideNumber = 0;
+      item.classList.forEach(c => {
+        if (c.startsWith('number-slide')) {
+          slideNumber = parseInt(c.substring(12));
+        }
+      });
 
-      // Allow previous class to render before applying new
-      setTimeout(function () {
-        item.classList.add('item-open');
-        item.focus();
-      }, 0);
-
-      // remove start position styles
-      setTimeout(function () {
-        item.classList.remove('item-open-start');
-      }, 200);
+      main.classList.add('open');
+      main.sliderElement?.focus();
+      main.setupSlider(slideNumber);
     }
   }
 
-  closeOnEsc(e: KeyboardEvent) {
+  keydown(e: KeyboardEvent) {
     if (e.keyCode == 27) { this.close(); }
+    if (e.keyCode == 37 && this.slider) { this.slider.prev(); }
+    if (e.keyCode == 39 && this.slider) { this.slider.next(); }
+  }
+
+  mouseWheel(e: MouseEvent) {
+    e.preventDefault();
+  }
+
+  next() {
+    if (this.slider) this.slider.next();
+  }
+
+  prev() {
+    if (this.slider) this.slider.prev();
   }
 
   close() {
-    this.querySelectorAll('.item').forEach(item => item.classList.remove('item-open'));
+    this.removeEventListener('wheel', this.mouseWheel);
+    if (this.slider) this.slider.destroy();
+    this.classList.remove('open');
   }
+
+  setupSlider(slide: number) {
+      if (!this.sliderElement) return;
+      this.slider = new KeenSlider(
+        this.sliderElement,
+        {
+          selector: '.item',
+          initial: slide,
+          slides: {
+            perView: 1,
+            spacing: 0,
+          },
+        },
+        []
+      );
+
+      this.slider.on("slideChanged", (slider) => {
+        const slide = slider.track.details.rel || 0;
+        const max = (slider.track.details.slides.length || 0)-1;
+        if (this.alElement) this.alElement.classList.toggle('disabled', slide <= 0);
+        if (this.arElement) this.arElement.classList.toggle('disabled', slide >= max);
+
+      })
+      this.addEventListener('wheel', this.mouseWheel);
+    }
 
 }
