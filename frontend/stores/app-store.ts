2d114eb4570d7919d7e282599a14e363f7c048a6
Sami Ekblad <sami@vaadin.com>
2022-11-28 01:25:25 +0200

    Updated page metadata

--- frontend/stores/app-store.ts
+++ frontend/stores/app-store.ts
@@ -2,10 +2,11 @@ import { makeAutoObservable } from 'mobx';
 
 export class AppStore {
   applicationName = 'Vaadin Add-on Directory';
-  appDescription = 'Find open-source widgets and components for your Vaadin application.';
+  appDescription = 'Find open-source widgets, add-ons, themes, and integrations for your Vaadin application.';
   appUrl = 'https://vaadin.com/directory/';
   appIcon = 'https://vaadin.com/images/trademark/PNG/VaadinLogomark_RGB_500x500.png';
-  currentViewTitle = '';
+  currentViewTitle = this.applicationName;
+  currentViewDescription = this.appDescription;
   searchViewScrollTop = 0;
 
   constructor() {
