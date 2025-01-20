fba744fe57419a1210df2a5a8ed2995d2c949ddc
Sami Ekblad <sami.ekblad@gmail.com>
2025-01-20 12:56:39 +0200

    Embed topics

--- frontend/views/addon/addon-view.ts
+++ frontend/views/addon/addon-view.ts
@@ -261,7 +261,7 @@ export class AddonView extends View implements BeforeEnterObserver {
 
         (function() {
           var d = document.createElement("script"); d.type = "text/javascript"; d.async = true;
-          d.src = DiscourseEmbed.discourseUrl + "javascripts/embed.js";
+          d.src = DiscourseEmbed.discourseUrl + "javascripts/embed-topics.js";
           (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(d);
         })();
       </script>`;
