2bd0413c4ef13ae83703e1272c88d0b2e4d0be56
Sami Ekblad <sami.ekblad@gmail.com>
2025-02-24 12:03:31 +0200

    Revert "Switch to Discourse comments."
    This reverts commit bff70a49204c1b4b320293a14241efc7a8ae5173.

--- frontend/views/addon/addon-view.ts
+++ frontend/views/addon/addon-view.ts
@@ -181,7 +181,7 @@ export class AddonView extends View implements BeforeEnterObserver {
             the <a href="https://discord.gg/MYFq5RTbBn" rel="noopened">chat on Discord</a> or
             <a href="https://stackoverflow.com/questions/tagged/vaadin" rel="noopened">ask questions on StackOverflow</a>.
           </p>
-          ${unsafeHTML(this.getDisourse())}
+          <iframe id="discussion-iframe" src="${this.getDiscussionLink()}"></iframe>
         </section>
 
       </section>
